98866f6bd7ad18e8446561904a2410db982c941c
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-03-30 18:53:35 +0100

    Implement pen, pencil, rainbow and eraser mode, clear grid function and pen/pencil color picker

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+// Add dark mode function
+// Let user download the drawing
+
 const DEFAULT_SIZE = 16;
-const DEFAULT_BG_COLOR = '#ffffff';
 const DEFAULT_GRID_BORDER = true;
+const DEFAULT_BG_COLOR = '#ffffff';
+const DEFAULT_PEN_COLOR = '#333333';
+const DEFAULT_MODE = 'pen';
 
 let currentSize = DEFAULT_SIZE;
-let current_bg_color = DEFAULT_BG_COLOR;
-let current_grid_border = DEFAULT_GRID_BORDER;
+let currentGridBorder = DEFAULT_GRID_BORDER;
+let currentBgColor = DEFAULT_BG_COLOR;
+let currentPenColor = DEFAULT_PEN_COLOR;
+let currentMode = DEFAULT_MODE;
+let mouseDown = false;
 
 const grid = document.querySelector('.grid');
 const bgColorPicker = document.querySelector('.bgColorPicker');
+const penColorPicker = document.querySelector('.penColorPicker');
+const penBtn = document.querySelector('.penBtn');
+const pencilBtn = document.querySelector('.pencilBtn');
+const rainbowBtn = document.querySelector('.rainbowBtn');
+const eraserBtn = document.querySelector('.eraserBtn');
+const clearBtn = document.querySelector('.clearBtn');
 const toggleBtn = document.getElementById('toggleBtn');
 const sliderLabel = document.getElementById('sliderLabel');
 const sliderInput = document.getElementById('slider');
 
-const toggleGrid = (current_grid_border) => {
+const toggleGrid = (currentGridBorder) => {
   const gridElements = grid.childNodes;
   for (const element of gridElements) {
-    if (current_grid_border === true) {
+    if (currentGridBorder === true) {
       element.classList.add('grid-element-border');
     } else {
       element.classList.remove('grid-element-border');
@@ -24,51 +38,127 @@ const toggleGrid = (current_grid_border) => {
 };
 
 const bgColorUpdate = () => {
-  grid.style.backgroundColor = current_bg_color;
+  const gridElements = grid.childNodes;
+  for (const element of gridElements) {
+    if (!(element.classList.contains('colored'))) {
+      element.style.backgroundColor = currentBgColor;
+    }
+  }
 };
 
-// This function loads the grid with a specific size
-// Could be with the default size of the grid (when the page is loaded) or with input from the user using the slider input
+const addColor = (type, target) => {
+  if (type === 'mouseover' && mouseDown === false) {
+    return;
+  }
+
+  switch (currentMode) {
+    case 'pen':
+      target.style.backgroundColor = currentPenColor;
+      target.classList.add('colored');
+      break;
+    case 'pencil':
+      target.style.backgroundColor = currentPenColor;
+      let targetOpacity = Number(target.style.opacity);
+      if (!(target.classList.contains('colored'))) {
+        targetOpacity = 0
+      }
+      if (!targetOpacity) {
+        targetOpacity = 0.1;
+      } else if (targetOpacity < 1) {
+        targetOpacity += 0.1;
+      }
+      target.style.opacity = targetOpacity;
+      target.classList.add('colored');
+      break;
+    case 'rainbow':
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      target.style.backgroundColor = `#${randomColor}`;
+      target.classList.add('colored');
+      break;
+    case 'eraser':
+      target.classList.remove('colored');
+      target.style.backgroundColor = currentBgColor;
+      target.style.opacity = 1;
+      break;
+  }
+}
+
 const loadGrid = (size) => {
   grid.innerHTML = '';
   grid.style.cssText = `grid-template-columns: repeat(${size}, 2fr); grid-template-rows: repeat(${size}, 2fr);`;
 
   for (let i = 0; i < (size * size); i++) {
     const gridElement = document.createElement('div');
+    gridElement.addEventListener('mouseover', (e) => {
+      addColor(e.type, e.target);
+    });
+    gridElement.addEventListener('mousedown', (e) => {
+      mouseDown = true;
+      addColor(e.type, e.target);
+    });
+    gridElement.addEventListener('mouseup', () => {
+      mouseDown = false;
+    });
     grid.appendChild(gridElement);
   }
-  toggleGrid(current_grid_border);
+  toggleGrid(currentGridBorder);
   bgColorUpdate();
 };
 
 bgColorPicker.addEventListener('input', (e) => {
-  current_bg_color = e.target.value
+  currentBgColor = e.target.value
   bgColorUpdate();
 });
 
-// It adds border to each element of the grid when clicked on
-toggleBtn.addEventListener('click', () => {
-  if (current_grid_border === true) {
-    current_grid_border = false;
-  } else {
-    current_grid_border = true;
+penColorPicker.addEventListener('input', (e) => {
+  currentPenColor = e.target.value;
+  if (currentMode === 'rainbow') {
+    currentMode = DEFAULT_MODE;
   }
-  toggleGrid(current_grid_border);
 });
 
-// It updates the slider label to match the input number
+penBtn.addEventListener('click', () => {
+  currentMode = 'pen';
+});
+
+pencilBtn.addEventListener('click', () => {
+  currentMode = 'pencil';
+});
+
+rainbowBtn.addEventListener('click', () => {
+  currentMode = 'rainbow';
+});
+
+eraserBtn.addEventListener('click', () => {
+  currentMode = 'eraser';
+});
+
+clearBtn.addEventListener('click', () => {
+  if (currentMode === 'eraser') {
+    currentMode = DEFAULT_MODE;
+  }
+  loadGrid(currentSize);
+});
+
+toggleBtn.addEventListener('click', () => {
+  if (currentGridBorder === true) {
+    currentGridBorder = false;
+  } else {
+    currentGridBorder = true;
+  }
+  toggleGrid(currentGridBorder);
+});
+
 const updateSliderLabel = (value) => {
   sliderLabel.textContent = `${value} x ${value}`;
 };
 
-// When the user changes the value of the input slider, it changes the current size of the grid and calls the loadgrid function with the current size, and also updates the slider label to match the input number
 sliderInput.addEventListener('input', (e) => {
   currentSize = e.target.value
   loadGrid(currentSize);
   updateSliderLabel(currentSize);
 });
 
-// When the page is loaded, it calls the loadGrid function with the default size of the grid
 window.onload = () => {
   loadGrid(DEFAULT_SIZE)
 }
