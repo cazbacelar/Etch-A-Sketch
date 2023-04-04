074991abaa24453027c6c6a1ab26e49f9082cff4
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-04-04 01:08:59 +0100

    fix opacity issue when using pen/rainbow mode on top of pencil

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 
 const DEFAULT_SIZE = 16;
 const DEFAULT_GRID_BORDER = true;
-const DEFAULT_BG_COLOR = '#ffffff';
-const DEFAULT_PEN_COLOR = '#333333';
-const DEFAULT_MODE = 'pen';
+const DEFAULT_BG_COLOR = "#ffffff";
+const DEFAULT_PEN_COLOR = "#333333";
+const DEFAULT_MODE = "pen";
 
 let currentSize = DEFAULT_SIZE;
 let currentGridBorder = DEFAULT_GRID_BORDER;
@@ -14,25 +14,25 @@ let currentPenColor = DEFAULT_PEN_COLOR;
 let currentMode = DEFAULT_MODE;
 let mouseDown = false;
 
-const grid = document.querySelector('.grid');
-const bgColorPicker = document.querySelector('.bgColorPicker');
-const penColorPicker = document.querySelector('.penColorPicker');
-const penBtn = document.querySelector('.penBtn');
-const pencilBtn = document.querySelector('.pencilBtn');
-const rainbowBtn = document.querySelector('.rainbowBtn');
-const eraserBtn = document.querySelector('.eraserBtn');
-const clearBtn = document.querySelector('.clearBtn');
-const toggleBtn = document.getElementById('toggleBtn');
-const sliderLabel = document.getElementById('sliderLabel');
-const sliderInput = document.getElementById('slider');
+const grid = document.querySelector(".grid");
+const bgColorPicker = document.querySelector(".bgColorPicker");
+const penColorPicker = document.querySelector(".penColorPicker");
+const penBtn = document.querySelector(".penBtn");
+const pencilBtn = document.querySelector(".pencilBtn");
+const rainbowBtn = document.querySelector(".rainbowBtn");
+const eraserBtn = document.querySelector(".eraserBtn");
+const clearBtn = document.querySelector(".clearBtn");
+const toggleBtn = document.getElementById("toggleBtn");
+const sliderLabel = document.getElementById("sliderLabel");
+const sliderInput = document.getElementById("slider");
 
 const toggleGrid = (currentGridBorder) => {
   const gridElements = grid.childNodes;
   for (const element of gridElements) {
     if (currentGridBorder === true) {
-      element.classList.add('grid-element-border');
+      element.classList.add("grid-element-border");
     } else {
-      element.classList.remove('grid-element-border');
+      element.classList.remove("grid-element-border");
     }
   }
 };
@@ -40,27 +40,28 @@ const toggleGrid = (currentGridBorder) => {
 const bgColorUpdate = () => {
   const gridElements = grid.childNodes;
   for (const element of gridElements) {
-    if (!(element.classList.contains('colored'))) {
+    if (!element.classList.contains("colored")) {
       element.style.backgroundColor = currentBgColor;
     }
   }
 };
 
 const addColor = (type, target) => {
-  if (type === 'mouseover' && mouseDown === false) {
+  if (type === "mouseover" && mouseDown === false) {
     return;
   }
 
   switch (currentMode) {
-    case 'pen':
+    case "pen":
       target.style.backgroundColor = currentPenColor;
-      target.classList.add('colored');
+      target.classList.add("colored");
+      target.style.opacity = 1;
       break;
-    case 'pencil':
+    case "pencil":
       target.style.backgroundColor = currentPenColor;
       let targetOpacity = Number(target.style.opacity);
-      if (!(target.classList.contains('colored'))) {
-        targetOpacity = 0
+      if (!target.classList.contains("colored")) {
+        targetOpacity = 0;
       }
       if (!targetOpacity) {
         targetOpacity = 0.1;
@@ -68,35 +69,36 @@ const addColor = (type, target) => {
         targetOpacity += 0.1;
       }
       target.style.opacity = targetOpacity;
-      target.classList.add('colored');
+      target.classList.add("colored");
       break;
-    case 'rainbow':
+    case "rainbow":
       const randomColor = Math.floor(Math.random() * 16777215).toString(16);
       target.style.backgroundColor = `#${randomColor}`;
-      target.classList.add('colored');
+      target.classList.add("colored");
+      target.style.opacity = 1;
       break;
-    case 'eraser':
-      target.classList.remove('colored');
+    case "eraser":
+      target.classList.remove("colored");
       target.style.backgroundColor = currentBgColor;
       target.style.opacity = 1;
       break;
   }
-}
+};
 
 const loadGrid = (size) => {
-  grid.innerHTML = '';
+  grid.innerHTML = "";
   grid.style.cssText = `grid-template-columns: repeat(${size}, 2fr); grid-template-rows: repeat(${size}, 2fr);`;
 
-  for (let i = 0; i < (size * size); i++) {
-    const gridElement = document.createElement('div');
-    gridElement.addEventListener('mouseover', (e) => {
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.addEventListener("mouseover", (e) => {
       addColor(e.type, e.target);
     });
-    gridElement.addEventListener('mousedown', (e) => {
+    gridElement.addEventListener("mousedown", (e) => {
       mouseDown = true;
       addColor(e.type, e.target);
     });
-    gridElement.addEventListener('mouseup', () => {
+    gridElement.addEventListener("mouseup", () => {
       mouseDown = false;
     });
     grid.appendChild(gridElement);
@@ -105,42 +107,42 @@ const loadGrid = (size) => {
   bgColorUpdate();
 };
 
-bgColorPicker.addEventListener('input', (e) => {
-  currentBgColor = e.target.value
+bgColorPicker.addEventListener("input", (e) => {
+  currentBgColor = e.target.value;
   bgColorUpdate();
 });
 
-penColorPicker.addEventListener('input', (e) => {
+penColorPicker.addEventListener("input", (e) => {
   currentPenColor = e.target.value;
-  if (currentMode === 'rainbow') {
+  if (currentMode === "rainbow") {
     currentMode = DEFAULT_MODE;
   }
 });
 
-penBtn.addEventListener('click', () => {
-  currentMode = 'pen';
+penBtn.addEventListener("click", () => {
+  currentMode = "pen";
 });
 
-pencilBtn.addEventListener('click', () => {
-  currentMode = 'pencil';
+pencilBtn.addEventListener("click", () => {
+  currentMode = "pencil";
 });
 
-rainbowBtn.addEventListener('click', () => {
-  currentMode = 'rainbow';
+rainbowBtn.addEventListener("click", () => {
+  currentMode = "rainbow";
 });
 
-eraserBtn.addEventListener('click', () => {
-  currentMode = 'eraser';
+eraserBtn.addEventListener("click", () => {
+  currentMode = "eraser";
 });
 
-clearBtn.addEventListener('click', () => {
-  if (currentMode === 'eraser') {
+clearBtn.addEventListener("click", () => {
+  if (currentMode === "eraser") {
     currentMode = DEFAULT_MODE;
   }
   loadGrid(currentSize);
 });
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener("click", () => {
   if (currentGridBorder === true) {
     currentGridBorder = false;
   } else {
@@ -153,12 +155,12 @@ const updateSliderLabel = (value) => {
   sliderLabel.textContent = `${value} x ${value}`;
 };
 
-sliderInput.addEventListener('input', (e) => {
-  currentSize = e.target.value
+sliderInput.addEventListener("input", (e) => {
+  currentSize = e.target.value;
   loadGrid(currentSize);
   updateSliderLabel(currentSize);
 });
 
 window.onload = () => {
-  loadGrid(DEFAULT_SIZE)
-}
+  loadGrid(DEFAULT_SIZE);
+};
